ea91facc6abede0f4dee7426722a53443cc7bebe
swat121 <stark2542@gmail.com>
2022-12-28 16:09:55 +0200

    Add test: bet check on the sports page

--- src/test/java/pm/academy/cases/AcademyTests.java
+++ src/test/java/pm/academy/cases/AcademyTests.java
@@ -1,13 +1,16 @@
 package pm.academy.cases;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.SoftAssertions;
 import org.testng.annotations.Test;
 import pm.academy.base.BaseTest;
-import pm.academy.pages.MainPage;
+import pm.academy.pages.*;
 
 public class AcademyTests extends BaseTest {
 
+    private static final Logger LOG = LogManager.getRootLogger();
     @Test
     public void mainMenuTest() {
         SoftAssertions softAssertions = new SoftAssertions();
@@ -22,5 +25,37 @@ public class AcademyTests extends BaseTest {
         Assertions.assertThat(new MainPage().isMenuButtonDisplayed())
                 .as("not displayed")
                 .isTrue();
+    }
+
+    @Test
+    public void setBetOnSportPageTest() {
+        SignUpPage signUpPage = new SignUpPage();
+        MainPage mainPage = new MainPage();
+        SportPage sportPage = new SportPage();
+        LoginPage loginPage = new LoginPage();
+        CouponPage couponPage = new CouponPage();
+
+        if (signUpPage.isSignUpPageLoaded()) {
+            signUpPage.clickCloseButton();
+            LOG.info("Authorization page was displayed and closed");
+        }
+
+        Assertions.assertThat(mainPage.isMenuButtonDisplayed()).as("Menu button is not displayed").isTrue();
+        mainPage.clickSportPage();
+
+        Assertions.assertThat(sportPage.isSportPageDisplayed()).as("Sport page is not displayed").isTrue();
+        sportPage.clickFootballTab();
+        //Двенадцать часов выберается,чтобы не было live игр и можно было нормально выбрать коефициент
+        sportPage.clickOneTwelveHoursButton();
+        sportPage.clickOnFirstNotSelectedCoef();
+
+        couponPage.clickOnAddToBetButton();
+
+        sportPage.clickOnFirstNotSelectedCoef();
+
+        couponPage.clickSelectedOutcomeCount();
+        couponPage.clickOnPlaceBetButton();
+
+        Assertions.assertThat(loginPage.isLoginPageLoaded()).as("Login page is not displayed").isTrue();
     }
 }
